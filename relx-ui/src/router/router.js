2450abfa6c9dbeac4d486df0da118803b3410bf4
mengweijin <1002284406@qq.com>
2021-01-30 17:38:48 +0800

    relx-ui

--- relx-ui/src/router/router.js
+++ relx-ui/src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '@/components/layout/Layout'
 import LayoutGoods from '@/components/layout/LayoutGoods'
 import LayoutGoodsType from '@/components/layout/LayoutGoodsType'
@@ -37,7 +37,7 @@ const routes = [
   ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes, 
 })
 
