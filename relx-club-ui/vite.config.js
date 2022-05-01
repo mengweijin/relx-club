62256a6d96e6bb4cd166f2dce60aa5cf51763b03
mengweijin <1002284406@qq.com>
2022-05-01 12:38:15 +0800

    update

--- relx-club-ui/vite.config.js
+++ relx-club-ui/vite.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
                 changeOrigin: true, // true/false, Default: false - changes the origin of the host header to the target URL
                     // 重写传过来的path路径，比如 `/api/index/1?id=10&name=zs`
                     //（注意:path路径最前面有斜杠（/），因此，正则匹配的时候不要忘了是斜杠（/）开头的；选项的 key 也是斜杠（/）开头的）
-                //rewrite: (path) => path.replace(/^\/api/, '')
+                rewrite: (path) => path.replace(/^\/api/, '')
             }
         }
     }
